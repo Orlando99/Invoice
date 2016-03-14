43853b5f8d74cbcfdf541eb430808eed7d30f8f2
Valeriy <mypowerismind@gmail.com>
2016-03-14 18:52:16 +0200

    Initial commit

---
+++ dist/js/sig.js
@@ -0,0 +1,1 @@
+$(function(){$("#sig").signature(),$("#clear").click(function(){$("#sig").signature("clear")}),$("#json").click(function(){alert($("#sig").signature("toJSON"))}),$("#svg").click(function(){alert($("#sig").signature("toSVG"))})});
