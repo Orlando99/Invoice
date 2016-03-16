5ffea82731db2c16d962c11d93c4384846fd4032
Valeriy <mypowerismind@gmail.com>
2016-03-16 15:35:21 +0200

    Merge all signups into 1 template

--- dist/js/angular/routes.js
+++ dist/js/angular/routes.js
@@ -5,17 +5,23 @@ angular.module('invoicesUnlimited')
 
 	$urlRouterProvider.otherwise('/signup');
 
+	var signUpIndex = {
+		name: 'signup',
+		url : "/signup",
+		controller : "SignUpVerificationController",
+		templateUrl : "./signUp/index.html"
+	}
+
+	var signUpIndexExtended = {
+		name : 'signup-extended',
+		url : "/signup/2",
+		controller : "SignUpVerificationController",
+		templateUrl : "./signUp/signup-2.html"
+	}
+
 	$stateProvider
-		.state('signup',{
-			url : "/signup",
-			controller : "SignUpVerificationController",
-			templateUrl : "./signUp/index.html"
-		})
-		.state('signup.verify',{
-			url : "/signup",
-			controller : "SignUpVerificationController",
-			templateUrl : "./../signUp/signup-2.html"
-		})
+		.state(signUpIndex)
+		.state(signUpIndexExtended)
 		.state('dashboard', {
 			url : "/home",
 			controller : "",
