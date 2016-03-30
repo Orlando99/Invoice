63d08926da4f05855cc3d44642dc430c669d043a
Valeriy <mypowerismind@gmail.com>
2016-03-30 18:31:22 +0300

    [Signup.AccountInfo]Add dropdown with average sale per customer

--- app/signup/account-info/AccountInfoController.js
+++ app/signup/account-info/AccountInfoController.js
@@ -3,11 +3,21 @@
 invoicesUnlimited.controller('AccountInfoController',['$scope','$state','userFactory','signUpFactory',
 	function($scope,$state,userFactory,signUpFactory){
 
+	$.validator.addMethod(
+		"AvgSaleRequired",
+		function(value,element){
+			return value != "avgSaleTitle";
+		}
+	)
+
 	$("#signUpForm").validate({
 		onkeyup : false,
 		onfocusout : false,
 		rules: {
-			avgSale 		: 'required',
+			avgSale 		: {
+				required : true,
+				AvgSaleRequired : true
+			},
 			monthlySales 	: 'required',
 			bankName 		: 'required',
 			routingNumber	: 'required',
@@ -22,6 +32,19 @@ invoicesUnlimited.controller('AccountInfoController',['$scope','$state','userFac
 			accountNumber	: 'Please specify your bank account number'			
 		}
 	});
+
+	$scope.avgSaleList = [];
+	var moneyVal = 0;
+	do {
+		if (!moneyVal) moneyVal = 1;
+		else if (moneyVal == 1) moneyVal = 5;
+		else if (moneyVal < 100) moneyVal += 5;
+		else if (moneyVal < 1000) moneyVal += 50;
+		else if (moneyVal < 10000) moneyVal += 500;
+		else moneyVal += 5000;
+		$scope.avgSaleList.push({value:"$ " + (moneyVal == 50000 ? moneyVal + "+" : moneyVal)});
+	} while(moneyVal != 50000)
+
 
 	if (userFactory.authorized) {
 		if (!userFactory.getBusinessInfo()) {
