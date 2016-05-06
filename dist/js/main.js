32d24f7c7882d3ca1f5fc55167296fee70eea66f
valercheck <mypowerismind@gmail.com>
2016-05-06 22:53:24 +0300

    Fix for color feature on App preferences

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,7 @@
+function Refresh(){
+	window.location.reload(true);
+}
+
 $(document).ready(function(){
 	function t(t){
 		$(t).css("width",100/(t.length-.44)+"%"),
@@ -16,13 +20,20 @@ $(document).ready(function(){
 	for(var e=".sales-list li",a=0;a<$(e).length;a++){var s=$(e+":nth-child("+(a+1)+") > h4").width(),l=$(e+":nth-child("+(a+1)+") > h3").width(),i=$(e+":nth-child("+(a+1)+")").width(),n=$(e+":nth-child("+(a+1)+") > .dotted-separator");n.css("width",i-(s+l+5)+"px")}$(".tabs li").on("click",function(){var t=$(this);$(".tabs li").removeClass("active"),t.addClass("active"),$(".tab").each(function(){$(this).attr("data-name")==t.attr("data-toggle")&&($(".tab").removeClass("active").hide(),$(this).addClass("active").show())})}),$(".button").on("click",function(){$(this).find(".submenu").toggleClass("showsub")}),$(".modal-opener").on("click",function(){var t=$(this),e=t.attr("data-toggle");$("."+e).addClass("show")}),$(".modal-close").on("click",function(){$(this).closest(".popup-modal").removeClass("show")}),$(".popup-modal").on("click",function(t){t.target==this&&$(".popup-modal").removeClass("show")});var o=new Date,c=(o.getDate(),o.getMonth()),h=o.getFullYear();$(".weekcalendar").fullCalendar({weekMode:"liquid",events:[{title:"Praent vestibulum",start:new Date(h,c,1,9,0),end:new Date(h,c,1,10,0),allDay:!1},{title:"Vestibulum iaculis lacinia",start:new Date(h,c,2,16,0),allDay:!1},{title:"Integer rutrum ante eu lacus",start:new Date(h,c,4,16,0),allDay:!1},{title:"Aliquam erat volpat. Duis ac turpis",start:new Date(h,c,9,16,0),allDay:!1},{title:"Donec in velit vel ipsum",start:new Date(h,c,10,16,0),allDay:!1}]}),$(".timesheet-more .table-row").on("click",function(){$(this).children(".timesheet-more-form").toggleClass("form-show")}),$('a[href=""]').on("click",function(t){t.preventDefault()}),$("body").on("click",function(t){t.target==this&&$(".submenu").removeClass("showsub")}),$(".button-next").on("click",function(){$(".input-container .messages").addClass("showmsg"),$(".input-container .messages .message").addClass("show")}),$('input[name="budget"]').change(function(){console.log($(this)),"yes"==$(this).attr("value")?$(".when-budget").removeClass("hide"):$(".when-budget").addClass("hide")}),$(".colors li").on("click",function(){$(".colors li").removeClass("active"),$(this).addClass("active")})
 	
 	if (typeof(Storage) !== "undefined") {
+		localStorage.removeItem('selectedAppColor');
 		var color = localStorage.getItem('applicationColor');
-		if (color) {
+		if (color && color != 'blue' && color != 'undefined') {
 			$('#appStyle').attr('href','./dist/css/main.' + color + '.css')
 		}
 	} else {
 		alert("No local storage");
 	}
+
+	$('body').on("click",'#saveAppPref',function(){
+		localStorage.setItem('applicationColor',localStorage.selectedAppColor);
+		localStorage.removeItem('selectedAppColor');
+		Refresh();
+	});
 
 });
 $(document).on("click",".organization",function(){
@@ -34,7 +45,11 @@ $(document).on("click",".blue,.cyan,.green,.yellow,.orange,.red,.pink,.purple",
 	function(elem){
 		var colorElem = $(elem.target);
 		if (typeof(Storage) !== "undefined") {
-			localStorage.setItem('applicationColor',colorElem.attr('class'));
+			localStorage.setItem('selectedAppColor',colorElem.attr('class'));
+			color = localStorage.selectedAppColor;
+			if (color && color != 'undefined') {
+				$('#appStyle').attr('href','./dist/css/main.' + color + '.css')
+			}
 		} else {
 			alert("No local storage");
 		}
