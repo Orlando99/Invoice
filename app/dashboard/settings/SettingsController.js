cfbac2bc890042c7a57fe8dd16d3d4b8466afd30
valercheck <mypowerismind@gmail.com>
2016-05-10 16:37:32 +0300

    Add select option on App preferences

--- app/dashboard/settings/SettingsController.js
+++ app/dashboard/settings/SettingsController.js
@@ -7,6 +7,19 @@ String.prototype.capitilize = function() {
 invoicesUnlimited.controller('SettingsController',['$scope','$state','userFactory',
 	function($scope,$state,userFactory){
 
+	if ($state.current.name == "dashboard.settings.app-preferences") {
+		if (typeof(Storage) !== "undefined") {
+		  	localStorage.removeItem('selectedAppColor');
+		  	var color = localStorage.getItem('applicationColor');
+		  	if (color && color != 'blue' && color != 'undefined') {
+		  	  	$('#appStyle').attr('href','./dist/css/main.' + color + '.css');
+		  	}
+		  	if (color) $('.colors li a.'+color).parent().addClass("active");
+		} else {
+		  	alert("No local storage");
+		}
+	}
+
 	if (!userFactory.authorized()) $state.go('login');
 
 	userFactory.loadAll(function(state){
