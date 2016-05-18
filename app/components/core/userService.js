9b9ee98f5c14a451129e66e232b7396db33e00db
valercheck <mypowerismind@gmail.com>
2016-05-18 22:50:16 +0300

    Extract User service from service.js

---
+++ app/components/core/userService.js
@@ -0,0 +1,46 @@
+'use strict';
+
+invoicesUnlimited.factory('userFactory',function(
+	principalFactory,accountFactory,businessFactory,$q){
+	
+	var currentUser = Parse.User.current() || {};
+	
+	function setLoginUserFields(){
+		currentUser.logout = function(){
+			return Parse.User.logOut().then(function(){
+				currentUser = {};
+				setEmptyUserFields();
+			});
+		};
+	}
+
+	function setEmptyUserFields(){
+		currentUser.login = function(params){
+			return Parse.User.logIn(params.username, params.password, {
+				success : function(user){
+					currentUser = user;
+					console.log("Logged in successfuly!");
+				},
+				error : function(user,error){
+					console.log(error.message);
+				}
+			});
+		}
+	}
+
+	function setCommonUserFields(){
+		currentUser.authorized = function(){
+			if (currentUser.id) return currentUser;
+			else return undefined;
+		}
+	}
+
+	function setUserFields(){
+		if (currentUser.id) setLoginUserFields();
+		else setEmptyUserFields();
+	}
+
+	setUserFields();
+	
+	return currentUser;
+});
