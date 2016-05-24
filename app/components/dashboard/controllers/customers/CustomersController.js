d6cf3dbe34b64eeb7d04160cc06102aedaa184e7
valercheck <mypowerismind@gmail.com>
2016-05-25 01:43:33 +0300

    [Customers] Bind Selected customer name to details page

--- app/components/dashboard/controllers/customers/CustomersController.js
+++ app/components/dashboard/controllers/customers/CustomersController.js
@@ -2,13 +2,22 @@
 
 invoicesUnlimited.controller('CustomersController',
 	function($scope,$state,userFactory,customerFactory,coreFactory,$controller,$q){
+
+	if ($state.current.name == 'dashboard.customers.details') {
+		$state.go('dashboard.customers.all');
+	}
 	
 	var def = $q.defer();
 
 	$controller('DashboardController',{$scope:$scope,$state:$state});
 
 	var user = userFactory.authorized();
+	$scope.selectedCustomer;
 	$scope.customers = [];
+
+	$scope.selectCustomer = function(item){
+		$scope.selectedCustomer = item;
+	}
 
 	$q.all([coreFactory.getAllCustomers()]).then(function(res){
 		res[0].forEach(function(elem){
