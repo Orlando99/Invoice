232a0369c3b7c33ccd7f5536c3272fc76224b1c0
adnanbinport <adnan@binaryport.com>
2016-07-13 12:11:37 +0500

    item settings (add/edit item)

--- app/components/dashboard/controllers/settings/ItemController.js
+++ app/components/dashboard/controllers/settings/ItemController.js
@@ -1,36 +1,143 @@
 'use strict';
 
 invoicesUnlimited.controller('ItemController',['$scope', '$state', '$controller', '$q',
-	'userFactory', 'coreFactory',
-	function($scope,$state,$controller,$q,userFactory, coreFactory){
+	'userFactory', 'coreFactory', 'taxService', 'itemService',
+function($scope,$state,$controller,$q,userFactory, coreFactory, taxService, itemService){
 
-	if(! userFactory.entity.length) {
-		console.log('User not logged in');
-		return undefined;
+if(! userFactory.entity.length) {
+	console.log('User not logged in');
+	return undefined;
+}
+
+var user = userFactory.entity[0];
+var organization = user.get("organizations")[0];
+$controller('DashboardController',{$scope:$scope,$state:$state});
+initalizeModalClasses();
+
+loadItemsAndTaxes();
+
+function loadItemsAndTaxes() {
+	showLoader();
+	var p = undefined;
+	var promises = [];
+
+	p = $q.when(coreFactory.getAllItems({
+		organization : organization
+	}))
+	.then(function(items) {
+		$scope.items = items;
+	});
+	promises.push(p);
+
+	p = taxService.getTaxes(user, function(taxes) {
+		$scope.taxes = taxes;
+	});
+	promises.push(p);
+
+	$q.all(promises).then(function() {
+		hideLoader();
+
+	}, function(error) {
+		console.log(error.message);
+		hideLoader();
+
+	});
+
+}
+
+function initializeScopeVariables() {
+	$scope.itemName = '';
+	$scope.itemRate = '';
+	$scope.itemDesc = '';
+	$scope.itemTax = undefined;
+}
+
+$scope.clearAddItemFields = function() {
+	initializeScopeVariables();
+}
+
+$scope.showItemDetail = function(index) {
+	$(".edit-item").addClass("show");
+	var item = $scope.items[index];
+
+	$scope.itemIndex = index;
+	$scope.itemName = item.entity.title;
+	$scope.itemRate = item.entity.rate;
+	$scope.itemDesc = item.entity.itemDescription;
+
+	if (item.tax) {
+		var taxes = $scope.taxes;
+		for (var i = 0; i < taxes.length; ++i) {
+			if (item.tax.id == taxes[i].id) {
+				$scope.itemTax = taxes[i];
+				break;
+			}
+		}
+	}
+}
+
+$scope.saveNewItem = function() {
+	showLoader();
+	var params = {
+		user : user,
+		organization : organization,
+		items : [{
+			title : $scope.itemName,
+			rate : $scope.itemRate,
+			tax : $scope.itemTax,
+			desc : $scope.itemDesc
+		}]
+	};
+
+	$q.when(coreFactory.getUserRole(user))
+	.then(function(role) {
+		var acl = new Parse.ACL();
+		acl.setRoleWriteAccess(role.get("name"), true);
+		acl.setRoleReadAccess(role.get("name"), true);
+
+		params.acl = acl;
+
+		return itemService.createItems(params);
+	})
+	.then(function(items) {
+		$scope.items.push(items[0]);
+		$(".new-item").removeClass("show");
+		hideLoader();
+
+	}, function(error) {
+		$(".new-item").removeClass("show");
+		hideLoader();
+		console.log(error.message);
+	});
+}
+
+$scope.saveEditedItem = function() {
+	var item = $scope.items[$scope.itemIndex];
+	if (! item) return;
+
+	showLoader();
+	item = item.entity;
+	item.set('title', $scope.itemName);
+	item.set('rate', String($scope.itemRate));
+	item.set('itemDescription', $scope.itemDesc);
+
+	if($scope.itemTax) {
+		item.set('tax', Parse.Object.extend("Tax")
+			.createWithoutData($scope.itemTax.id));
+	} else {
+		item.unset('tax');
 	}
 
-	var user = userFactory.entity[0];
-	$controller('DashboardController',{$scope:$scope,$state:$state});
-	initalizeModalClasses();
+	item.save().then(function() {
+		$state.reload();
 
-	loadItems();
+	}, function(error) {
+		$(".edit-item").removeClass("show");
+		hideLoader();
+		console.log(error.message);
+	})
 
-	function loadItems() {
-		showLoader();
-		var organization = user.get("organizations")[0];
-		$q.when(coreFactory.getAllItems({
-			organization : organization
-		}))
-		.then(function(items) {
-			$scope.items = items;
-			hideLoader();
-
-		}, function(error) {
-			console.log(error.message);
-			hideLoader();
-
-		});
-	}
+}
 
 //----
 /*
